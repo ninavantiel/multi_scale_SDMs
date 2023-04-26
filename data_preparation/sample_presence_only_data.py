771bfa2b71d96fabd248e3524f396c0c5594521e
Nina van Tiel <nina.vantiel@gmail.com>
2023-04-26 17:46:13 +0200

    f1 score, weighted loss

--- data_preparation/sample_presence_only_data.py
+++ data_preparation/sample_presence_only_data.py
@@ -4,13 +4,12 @@ from sklearn.model_selection import train_test_split
 data_path = 'data/full_data/'
 presence_only_path = data_path+'Presence_only_occurrences/Presences_only_train.csv'
 presence_only_sampled_path = data_path+'Presence_only_occurrences/Presences_only_train_sampled.csv'
-presence_only_sampled_train_path = data_path+'Presence_only_occurrences/Presences_only_sampled_1_3_train.csv'
-presence_only_sampled_val_path = data_path+'Presence_only_occurrences/Presences_only_sampled_1_3_val.csv'
+presence_only_sampled_100_path = data_path+'Presence_only_occurrences/Presences_only_train_sampled_100.csv'
 
 df = pd.read_csv(presence_only_path, sep=';')
 print(f"Shape of presence only data: ", df.shape)
 
-# thresh = 500
+# thresh = 100
 # frac = 0.5
 # species_counts = df.groupby('speciesId').glcID.count().sort_values()
 # df_no_sample = df[df['speciesId'].isin(species_counts[species_counts <= thresh].index)]
@@ -22,9 +21,15 @@ print(f"Shape of presence only data: ", df.shape)
 # df_sampled.to_csv(presence_only_sampled_path, sep=';')
 # print(f"Sampled presence only data saved!")
 
-df_sample = df.groupby('speciesId', group_keys=False).apply(lambda x: x.sample(frac=0.3))
-train_data, val_data = train_test_split(df_sample, test_size=0.2)
+thresh = 100
+species_counts = df.groupby('speciesId').glcID.count().sort_values()
+df_no_sample = df[df['speciesId'].isin(species_counts[species_counts <= thresh].index)]
+df_sample = df[df['speciesId'].isin(species_counts[species_counts > thresh].index)].groupby(
+    'speciesId', group_keys=False).apply(lambda x: x.sample(thresh))
+df_sampled = pd.concat([df_no_sample, df_sample])
+print(f"Shape of *sampled* presence only data: ", df_sampled.shape)
 
-train_data.to_csv(presence_only_sampled_train_path, sep=';')
-val_data.to_csv(presence_only_sampled_val_path, sep=';')
-print(f"Sampled presence only train and validation split data saved!")
+df_sampled.to_csv(presence_only_sampled_100_path, sep=';')
+print(f"Sampled presence only data saved!")
+
+
--- simple_cnn.py
+++ simple_cnn.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, f1_score
 import wandb
 
 from GLC23PatchesProviders import MultipleRasterPatchProvider, RasterPatchProvider#, JpegPatchProvider
@@ -20,7 +20,7 @@ from models import cnn
 # OCCURRENCE DATA
 data_path = 'data/full_data/'
 # presence_only_path = data_path+'Presence_only_occurrences/Presences_only_train.csv'
-presence_only_path = data_path+'Presence_only_occurrences/Presences_only_train_sampled.csv'
+presence_only_path = data_path+'Presence_only_occurrences/Presences_only_train_sampled_100.csv'
 presence_absence_path = data_path+'Presence_Absence_surveys/Presences_Absences_train.csv'
 
 # COVARIATES
@@ -28,10 +28,10 @@ presence_absence_path = data_path+'Presence_Absence_surveys/Presences_Absences_t
 # p_soil = MultipleRasterPatchProvider(data_path+'EnvironmentalRasters/Soilgrids/') #9
 # n_features = 15 #44 
 
-run_name = '2504_1700_full_data_sampled'
+run_name = '2604_1730_full_data_sampled_100_f1_weighted_loss'
 if not os.path.exists(f"models/{run_name}"): os.makedirs(f"models/{run_name}")
 n_epochs = 100
-batch_size = 256
+batch_size = 512
 seed = 42
 
 np.random.seed(seed) # Numpy seed also uses by Scikit Learn
@@ -51,55 +51,48 @@ if __name__ == "__main__":
     p_hfp_d = MultipleRasterPatchProvider(data_path+'EnvironmentalRasters/HumanFootprint/detailed/') #14
     p_hfp_s = RasterPatchProvider(data_path+'EnvironmentalRasters/HumanFootprint/summarized/HFP2009_WGS84.tif') #1
 
-    # PRESENCE ONLY DATA
+    # PRESENCE ONLY DATA: train dataset
     presence_only_df = pd.read_csv(presence_only_path, sep=";", header='infer', low_memory=False)
-    # split presence only dataset into train and validation sets
-    train_df, val_df = train_test_split(presence_only_df, test_size=0.2)
-
-    # train dataset
     train_data = PatchesDatasetMultiLabel(
-        occurrences=train_df.reset_index(), 
+        occurrences=presence_only_df.reset_index(), 
         providers=(p_bioclim, p_hfp_d, p_hfp_s)
     )
     print(f"\nTRAINING DATA: n={len(train_data)}")
-
     # get number of features and number of species in train dataset
     n_features = train_data[0][0].cpu().detach().shape[0]
     print(f"Number of covariates = {n_features}")
     n_species = len(train_data.unique_sorted_targets)
     print(f"Number of species = {n_species}")
 
-    # validation dataset
-    val_data = PatchesDatasetMultiLabel(
-        occurrences=val_df.reset_index(), 
-        providers=(p_bioclim, p_hfp_d, p_hfp_s),
-        ref_targets=train_data.unique_sorted_targets
-    )
-    print(f"VALIDATION DATA: n={len(val_data)}")
-
-    # PRESENCE ABSENCE DATA 
+    # PRESENCE ABSENCE DATA: validation dataset
     presence_absence_df = pd.read_csv(presence_absence_path, sep=";", header='infer', low_memory=False)
-    # test dataset
-    test_data = PatchesDatasetMultiLabel(
+    val_data = PatchesDatasetMultiLabel(
         occurrences=presence_absence_df, 
         providers=(p_bioclim, p_hfp_d, p_hfp_s),
         ref_targets=train_data.unique_sorted_targets
     )
-    print(f"TEST DATA: n={len(test_data)}")
+    print(f"TEST DATA: n={len(val_data)}")
 
     # data loaders
     train_loader = torch.utils.data.DataLoader(train_data, shuffle=True, batch_size=batch_size, num_workers=16)
-    val_loader = torch.utils.data.DataLoader(val_data, shuffle=False, batch_size=batch_size, num_workers=4)
-    test_loader = torch.utils.data.DataLoader(test_data, shuffle=False, batch_size=batch_size, num_workers=4)
+    val_loader = torch.utils.data.DataLoader(val_data, shuffle=False, batch_size=batch_size*10, num_workers=16)
 
     # model
     model = cnn(n_features, n_species).to(dev)
     print(f"\nMODEL: {model}")
 
-    loss_fn = torch.nn.BCEWithLogitsLoss()
-    # loss_fn = torch.nn.BCEWithLogitsLoss(pos_weight=torch.tensor(weight)).to(device)(pred, target)
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    # loss function
+    # train_labels = torch.cat([torch.unsqueeze(train_data[i][1], dim=0) for i in range(len(train_data))])
+    # weights = (train_labels.shape[0] - torch.sum(train_labels, axis=0)) / (torch.sum(train_labels, axis=0) + 1e-3)
+    weights = torch.tensor((
+        (len(train_data) - presence_only_df.groupby('speciesId').glcID.count()) / (presence_only_df.groupby('speciesId').glcID.count()+ 1e-3)
+    ).values)
+    # loss_fn = torch.nn.BCEWithLogitsLoss()
+    loss_fn = torch.nn.BCEWithLogitsLoss(pos_weight=torch.tensor(weights)).to(dev)#(pred, target)
     min_loss = 99999
+
+    # optimizer
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
     run = wandb.init(
         project='geolifeclef23', entity="deephsm", name=run_name, 
@@ -120,8 +113,9 @@ if __name__ == "__main__":
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
         train_loss = loss.cpu().detach()
-        print(f"{epoch}) LOSS={train_loss}")
+        print(f"{epoch}) TRAIN LOSS={train_loss}")
 
         # model checkpoint
         torch.save({
@@ -141,45 +135,32 @@ if __name__ == "__main__":
             }, f"models/{run_name}/best.pth")
 
         model.eval()
-        avg_aucs = []
-        for inputs, labels in val_loader:
+        # val_loss_list = []
+        y_true_list = []
+        y_pred_list = []
+        for inputs, labels in tqdm(val_loader):
             inputs = inputs.to(dev)
             labels = labels.to(dev)
             y_pred = model(inputs)
-            # validation loss
-            val_loss = loss_fn(y_pred, labels).cpu().detach()
-            # validation AUC
-            y_pred = y_pred.cpu().detach().numpy()
-            y_true = labels.cpu().detach().numpy()
-            species_idx = np.where(y_true.sum(axis=0) != 0)[0]
-            auc_rocs = roc_auc_score(y_true[:,species_idx], y_pred[:,species_idx], average=None)
-            avg_aucs.append(auc_rocs.mean())
-        # average AUC 
-        avg_auc = np.array(avg_aucs).mean() # this is not great
-        print(f"{epoch}) AVG_AUC={avg_auc} (for {len(species_idx)} species)")
+            y_true_list.append(labels)
+            y_pred_list.append(y_pred)
+        
+        # validation loss
+        y_true = torch.cat(y_true_list)
+        y_pred = torch.cat(y_pred_list)
+        val_loss = loss_fn(y_pred, y_true).cpu().detach()
+        print(f"\tVALIDATION LOSS={val_loss}")
+
+        # optimise threshold for f1-score
+        y_true = y_true.detach().numpy()
+        y_pred = y_pred.detach().numpy()
+        thresholds = np.arange(0, 0.001, 1e-4)
+        f1_scores = []
+        for thresh in thresholds:
+            y_bin = np.where(y_pred > thresh, 1, 0)
+            f1_scores.append(np.array([f1_score(y_true[i,:], y_bin[i,:]) for i in range(y_true.shape[0])]).mean())
+        best_threshold = thresholds[np.argmax(f1_scores)]
+        best_f1 = np.max(f1_scores)       
 
         # log train and val metrics for epoch
-        wandb.log({"train_loss": train_loss, "val_loss": val_loss, "val_avg_auc": avg_auc})
-        if epoch == 0: wandb.config['n_species_val'] = len(species_idx)
-    
-    model.eval()
-    avg_aucs = []
-    for inputs, labels in test_loader:
-        inputs = inputs.to(dev)
-        labels = labels.to(dev)
-        y_pred = model(inputs)
-        # test loss
-        test_loss = loss_fn(y_pred, labels).cpu().detach()
-        # test AUC
-        y_pred = y_pred.cpu().detach().numpy()
-        y_true = labels.cpu().detach().numpy()
-        species_idx = np.where(y_true.sum(axis=0) != 0)[0]
-        auc_rocs = roc_auc_score(y_true[:,species_idx], y_pred[:,species_idx], average=None)
-        avg_aucs.append(auc_rocs.mean())
-
-    avg_auc = np.array(avg_aucs).mean()
-    print(f"TEST SET: AVG_AUC={avg_auc} (for {len(species_idx)} species)")
-
-    wandb.log({"test_loss": test_loss, "test_avg_auc": avg_auc})
-    wandb.config["n_species_test"] = len(species_idx)
-        
+        wandb.log({"train_loss": train_loss, "val_loss": val_loss, "val_best_threshold": best_threshold, "val_best_f1": best_f1})        
